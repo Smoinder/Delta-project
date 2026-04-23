a8bf95e7a6e51355d2a1ee9dd9770a9b7652e94c
alexander.zhuravlev <zhur06@gmail.com>
2026-04-23 11:48:03 +0200

    feat: Hidden data in GameEngine

--- DeltaProject/src/main/java/PipesInTheDesert/GameEngine.java
+++ DeltaProject/src/main/java/PipesInTheDesert/GameEngine.java
@@ -1,5 +1,6 @@
 package PipesInTheDesert;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import PipesInTheDesert.Connectors.Pipe;
@@ -15,26 +16,83 @@ import PipesInTheDesert.Players.Saboteur;
  */
 public class GameEngine {
     /** Player whose turn is currently active. */
-    public Player activePlayer;
+    private Player _activePlayer;
     /** All cisterns participating in the game. */
-    public List<Cistern> cisterns;
+    private final List<Cistern> _cisterns = new ArrayList<>();
     /** All pumps participating in the game. */
-    public List<Pump> pumps;
+    private final List<Pump> _pumps = new ArrayList<>();
     /** All springs participating in the game. */
-    public List<Spring> springs;
+    private final List<Spring> _springs = new ArrayList<>();
     /** All pipes participating in the game. */
-    public List<Pipe> pipes;
+    private final List<Pipe> _pipes = new ArrayList<>();
     /** Plumber players in turn order. Acts as FIFO */
-    public List<Plumber> plumbers;
+    private final List<Plumber> _plumbers = new ArrayList<>();
     /** Saboteur players in turn order. Acts as FIFO */
-    public List<Saboteur> saboteurs;
+    private final List<Saboteur> _saboteurs = new ArrayList<>();
     /** Total score accumulated by plumbers. */
     public int plumbersScore;
     /** Total score accumulated by saboteurs. */
     public int saboteursScore;
     /** Current turn index of the game. */
     public int turnNumber;
+
     private Mode mode;
+
+    public void setActivePlayer(Player p) {
+        this._activePlayer = p;
+    }
+
+    public Player getActivePlayer() {
+        return this._activePlayer;
+    }
+
+    public void addCistern(Cistern c) {
+        this._cisterns.add(c);
+    }
+
+    public List<Cistern> getCisterns() {
+        return  this._cisterns;
+    }
+
+    public void addPump(Pump p) {
+        this._pumps.add(p);
+    }
+
+    public List<Pump> getPumps() {
+        return this._pumps;
+    }
+
+    public void addSpring(Spring s) {
+        this._springs.add(s);
+    }
+
+    public List<Spring> getSprings() {
+        return this._springs;
+    }
+
+    public void addPipe(Pipe p) {
+        this._pipes.add(p);
+    }
+
+    public List<Pipe> getPipes() {
+        return this._pipes;
+    }
+
+    public void addPlumber(Plumber p) {
+        this._plumbers.add(p);
+    }
+
+    public List<Plumber> getPlumbers() {
+        return this._plumbers;
+    }
+
+    public void addSaboteur(Saboteur s) {
+        this._saboteurs.add(s);
+    }
+
+    public List<Saboteur> getSaboteurs() {
+        return this._saboteurs;
+    }
 
     public void setMode(Mode mode) {
         this.mode = mode;
